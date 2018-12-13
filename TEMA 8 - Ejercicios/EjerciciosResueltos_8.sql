5b94b75aafbad164c92f6bd0e7df5bc4612d364f
Guillermo Sesé Santos <guillermosesesantos@hotmail.com>
2018-12-13 09:03:54 +0100

    ejemplo procedures

--- TEMA 8 - Ejercicios/EjerciciosResueltos_8.sql
+++ TEMA 8 - Ejercicios/EjerciciosResueltos_8.sql
@@ -29,3 +29,35 @@ BEGIN
     DBMS_OUTPUT.PUT_LINE(v_app_emple || ' - ' || v_of_emple);
 END;
 /
+
+
+-- Ejemplo de PROCEDURE, 
+-- solo se le pone parentesis cuando se le pasa un parametro
+
+CREATE PROCEDURE BORRAR_DPTO
+IS 
+    v_num_empleados NUMBER(2);
+BEGIN
+    INSERT INTO depart VALUES (99, 'DPTO DE LOLO',NULL);
+    UPDATE emple SET dept_no = 99
+    WHERE dept_no = 20;
+    v_num_empleados := SQL%ROWCOUNT;
+
+    DELETE FROM depart WHERE dept_no = 20;
+    DBMS_OUTPUT.PUT_LINE(v_num_empleados || ' Empleados ubicados en PROVISIONAL');
+    DBMS_OUTPUT.PUT_LINE('Dept 20 borrado con exito');
+
+EXCEPTION
+    WHEN OTHERS THEN
+        RAISE_APPLICATION_ERROR (-20000, 'Error en la aplicación');
+END;
+/
+
+-- Para ejecutar 
+EXECUTE BORRAR_DPTO();
+
+-- Para borrar
+DROP PROCEDURE BORRAR_DPTO();
+
+-- Para ver los errores de compliacion
+SHOW ERRORS
