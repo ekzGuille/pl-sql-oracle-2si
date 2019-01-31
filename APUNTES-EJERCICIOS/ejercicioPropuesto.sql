ba925c8c5c3e8cfd0af2af7b28cfa327c93dabdc
ekzGuille <guillermosesesantos@hotmail.com>
2019-01-31 09:55:19 +0100

    enunciado Practica

---
+++ APUNTES-EJERCICIOS/ejercicioPropuesto.sql
@@ -0,0 +1,13 @@
+-- Cliente puede hacer pedidos
+
+-- Verificar: 
+  -- Cliente tiene cuenta
+  -- Cliente tiene dinero en la cuenta
+  -- Hay un pedido sobre el producto
+
+-- (Por cada tabla crear un Type Record para trabajar con esos valores)
+
+-- Un cliente tiene cuentas
+-- Una cuenta puede estar a nombre de varios clientes
+-- Una venta se realiza sobre pedido
+-- El cliente tiene que tener saldo en su cuenta para poder gestionar el pedido
